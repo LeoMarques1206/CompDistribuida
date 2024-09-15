4a819d47da34565c95a56575d83ed92340438ecb
Leonardo Marques <111006186+LeoMarques1206@users.noreply.github.com>
2024-09-15 15:35:03 -0300

    Update AV1.py

--- Trabalho1-CompDist/AV1.py
+++ Trabalho1-CompDist/AV1.py
@@ -15,20 +15,21 @@ def disponibilidade(n, k, p):
 # (p**i) -> Isso representa a probabilidade de exatamente i servidores estarem indisponiveis.  Cada servidor tem probabilidade p de estar disponível, e a probabilidade de i servidores estarem disponíveis é p multiplicado por si mesmo i vezes, ou p^i .
 # ((1 - p)**(n - i)) -> A probabilidade de um servidor esta disponivel é 1 - p, entao para que n - i servidores estejam disponiveis, multiplicamos (1-p) por si mesmo n - i vezes, ou seja ((1 - p)**(n - i))
 
-plt.figure(figsize=(8, 5))
 
-N = [3, 5]
-K = [1, 3]
-P = np.linspace(0.1, 0.9, 100) 
+plt.figure(figsize=(10, 6))
+
+N = [3, 5] 
+P = np.linspace(0.1, 0.9, 100)  
 
 for n in N:
-    for k in K:
+    ks = [1, n, (n//2) + 1] 
+    for k in ks:
         disponibilidade_values = [disponibilidade(n, k, p) for p in P]
         plt.plot(P, disponibilidade_values, label=f'n={n}, k={k}')
 
-plt.title('Disponibilidade do Serviço para Diferentes Valores de n e k')
+plt.title('Disponibilidade do Serviço para k=1, k=n, e k=(n/2)+1')
 plt.xlabel('Probabilidade de Disponibilidade de um Servidor (p)')
 plt.ylabel('Disponibilidade do Serviço')
-plt.legend()
-plt.grid(True)  
+plt.legend(title="Configurações (n, k)")
+plt.grid(True) 
 plt.show()
